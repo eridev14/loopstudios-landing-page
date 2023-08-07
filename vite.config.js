ee92b593c40559dd975237335dbac8c379f473c5
eridev14 <eridev14hc@gmail.com>
2023-08-07 18:55:53 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/loopstudios-landing-page/'
+  base: 'https://eridev14.github.io/loopstudios-landing-page/'
 })
